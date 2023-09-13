68925a2e24e370e239ad5c391d44944ae28e6890
“James <S3900781@student.rmit.edu.au>
2023-09-13 14:55:47 +1000

    add cross origin

--- backend/SuperPrice/src/main/java/superPrice/storePrice/StorePriceController.java
+++ backend/SuperPrice/src/main/java/superPrice/storePrice/StorePriceController.java
@@ -1,6 +1,7 @@
 package superPrice.storePrice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import superPrice.storePrice.store.Store;
 
 import java.util.Collection;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "price")
 public class StorePriceController {
