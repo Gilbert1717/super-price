f131f85ad3eab61f109bf7c2b4d77f9b419663c4
gilbert <s3665887@student.rmit.edu.au>
2023-09-19 17:52:24 +1000

    init branch

--- backend/SuperPrice/src/main/java/superPrice/orders/controller/OrderController.java
+++ backend/SuperPrice/src/main/java/superPrice/orders/controller/OrderController.java
@@ -1,4 +1,11 @@
 package superPrice.orders.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "price")
 public class OrderController {
 }
